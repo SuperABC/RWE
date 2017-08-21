ee0e4e3bdd1f4722a0cb99014cd306c68df9a552
SuperABC <1363028533@qq.com>
2017-08-21 23:51:03 +0800

    Add files via upload

--- Window/scene/eye.cpp
+++ Window/scene/eye.cpp
@@ -1,6 +1,9 @@
 #include "Frame/main/main.h"
 #include "eye.h"
 
+extern int SCR_WIDTH;
+extern int SCR_HEIGHT;
+
 Eye::Eye() {
 	this->pos = glm::vec3(0, 0, 4);
 	this->dir = glm::vec3(0, 0, -4);
@@ -8,14 +11,16 @@ Eye::Eye() {
 	this->theta = float(PI / 2);
 	this->phy = float(PI / 2);
 	this->dist = 8.f;
+	this->model = glm::mat4();
 }
 Eye::Eye(glm::vec3 pos) {
 	this->pos = pos;
-	this->dir = glm::vec3(0, 0, -1);
+	this->dir = -pos;
 	this->up = glm::vec3(0, 1, 0);
 	this->theta = float(PI / 2);
 	this->phy = float(PI / 2);
 	this->dist = 8.f;
+	this->model = glm::mat4();
 }
 Eye::~Eye() {
 
@@ -24,24 +29,30 @@ Eye::~Eye() {
 glm::mat4 Eye::view() {
 	return glm::lookAt(this->pos, this->pos + this->dir, this->up);
 }
+void Eye::move(float dx, float dy) {
+	pos.x -= dy*cos(theta) + dx*sin(theta);
+	pos.z -= dy*sin(theta) - dx*cos(theta);
+}
 void Eye::rotate(float angx, float angy) {
+	float atx = pos.x + dir.x;
+	float aty = pos.y + dir.y;
+	float atz = pos.z + dir.z;
 	this->theta += angx;
 	this->phy = clamp<float>(0.01f, float(PI - 0.01), this->phy - angy);
-	this->pos.x = dist*sin(phy)*cos(theta);
-	this->pos.y = dist*cos(phy);
-	this->pos.z = dist*sin(phy)*sin(theta);
-	this->dir.x = -this->pos.x;
-	this->dir.y = -this->pos.y;
-	this->dir.z = -this->pos.z;
+	this->pos.x = atx + dist*sin(phy)*cos(theta);
+	this->pos.y = aty + dist*cos(phy);
+	this->pos.z = atz + dist*sin(phy)*sin(theta);
+	this->dir.x = atx-this->pos.x;
+	this->dir.y = aty-this->pos.y;
+	this->dir.z = atz-this->pos.z;
 }
 void Eye::zoom(float prop) {
-	dist = clamp<float>(.25f, 64.f, dist*prop);
-	this->pos.x = dist*sin(phy)*cos(theta);
-	this->pos.y = dist*cos(phy);
-	this->pos.z = dist*sin(phy)*sin(theta);
-	this->dir.x = -this->pos.x;
-	this->dir.y = -this->pos.y;
-	this->dir.z = -this->pos.z;
+	float preDist = dist;
+	dist = clamp<float>(.1f, 256.f, dist*prop);
+
+	prop = dist / preDist;
+	this->pos += dir * (1 - prop);
+	this->dir *= prop;
 }
 Ray Eye::ray(int x, int y) {
 	Ray ret = Ray();
@@ -51,6 +62,6 @@ Ray Eye::ray(int x, int y) {
 	float fy = 2 * float(SCR_HEIGHT/2 - y)/SCR_HEIGHT;
 	glm::mat4 projection = glm::perspective(45.f, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 	glm::mat4 view = glm::lookAt(pos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
-	ret.dir = Point(glm::inverse(view)*glm::inverse(projection)*glm::vec4(fx, fy, 1.f, 1.f))-Point(pos.x, pos.y, pos.z);
+	ret.dir = Point(glm::inverse(projection*view)*glm::vec4(fx, fy, 1.f, 1.f)) - Point(pos.x, pos.y, pos.z);
 	return ret;
 }
