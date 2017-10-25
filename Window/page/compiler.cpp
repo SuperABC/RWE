5755c9f2772427c3f668c9da6b5aa32e82f5428c
SG master <1363028533@qq.com>
2017-10-25 11:25:43 +0800

    Update

---
+++ Window/page/compiler.cpp
@@ -0,0 +1,31 @@
+#include "layout.h"
+
+extern vector<Layout*> window;
+extern vector<Scene*> globe;
+
+extern Mouse *mouse;
+
+Compiler::Compiler() {
+	type = LT_COMPILER;
+}
+void Compiler::keyDown(int code) {
+
+}
+void Compiler::keyUp(int code) {
+
+}
+void Compiler::mouseMove(Ray r) {
+
+}
+void Compiler::mouseDrag(Ray r, Ray pre) {
+
+}
+void Compiler::mouseClick(int button, int state, Ray r) {
+
+}
+void Compiler::mouseWheel(int wheel, int dir, Ray r) {
+
+}
+void Compiler::idle() {
+
+}
