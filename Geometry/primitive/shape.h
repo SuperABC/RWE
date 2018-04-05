96259ec437ba1fcb5240af6515fa7bfdb417269e
SG master <1363028533@qq.com>
2018-04-05 13:25:22 +0800

    First attempt.

--- Geometry/primitive/shape.h
+++ Geometry/primitive/shape.h
@@ -15,12 +15,15 @@ private:
 	int widthPixel, heightPixel;
 	Vector norm;
 public:
-	Square() {};
+	Square() {
+		p1 = p2 = p3 = p4 = Point(0.f, 0.f, 0.f);
+	};
 	Square(Point p1, Point p2, Point p3, Point p4, int width = 100, int height = 100) :
 		p1(p1), p2(p2), p3(p3), p4(p4), widthPixel(width), heightPixel(height) {
 		norm = cross(p2 - p1, p3 - p1);
 	};
 	~Square() {};
+	glm::vec2 getSize();
 
 	Box bound();
 	glm::vec2 intersect(Ray r);
