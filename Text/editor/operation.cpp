c4d3e9ea6850e987eddbf5f906164697f355eedf
SG master <1363028533@qq.com>
2018-04-14 21:17:35 +0800

    Update.

---
+++ Text/editor/operation.cpp
@@ -0,0 +1,21 @@
+#include "Text/editor/operation.h"
+
+Operation::Operation() {
+
+}
+Operation::~Operation() {
+
+}
+
+void Operation::add(int block, int line, int offset, char ch) {
+
+}
+void Operation::add(int block, int line, int offset, std::vector<std::string> str) {
+
+}
+void Operation::del(int block, int line, int offset, char ch) {
+
+}
+void Operation::del(int block, int line, int offset, std::vector<std::string> str) {
+
+}
