3905705cfc5c28cf2dd05d0a13bd2661ac6cdb74
SG master <1363028533@qq.com>
2018-04-26 15:05:59 +0800

    Update.

--- Window/scene/eye.h
+++ Window/scene/eye.h
@@ -10,17 +10,21 @@ public:
 	Eye(glm::vec3 pos);
 	~Eye();
 
-	glm::vec3 pos, dir, up;
+	glm::vec3 pos, center, dir, up;
 	float theta, phy;
 	float dist;
 
 	glm::mat4 model;
 
+	void sync(float theta, float phy);
+	void sync(glm::vec3 dir);
 	glm::mat4 view();
 	void move(float dx, float dy);
 	void rotate(float angx, float angy);
 	void zoom(float prop);
 	Ray ray(int x, int y);
+
+	void animate(glm::vec3 pos, glm::vec3 dir);
 };
 
 #endif
